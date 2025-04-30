2ea292666238b783e0f6ecb6324a6570ef27f5e0
Yevhen <e_danilchenko@outlook.com>
2025-04-30 17:57:16 +0300

    Initial Commit

---
+++ music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by macbook on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
